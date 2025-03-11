b350d49743082c0159d65cab7e978aef442a49b2
Arun Allanki <allanki.arun@gmail.com>
2025-03-11 20:42:48 +0530

    updated

--- src/screens/Admin.js
+++ src/screens/Admin.js
@@ -264,7 +264,6 @@ export default function Admin() {
         (order) =>
           `${order.OrderId}`.includes(search) && order.Completed === false
       );
-      console.log(searchedOrders, "Searched Orders ive ra ayya");
 
       return (
         <div className="AdminPage">
